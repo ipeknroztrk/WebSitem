32b159adc5245b1b83bb7094b6ba48c73b641db3
ipek nur <114228895+ipeknroztrk@users.noreply.github.com>
2025-11-29 01:20:35 +0300

    Update Program.cs

--- MyPortfolıoUdemy/Program.cs
+++ MyPortfolıoUdemy/Program.cs
@@ -1,7 +1,28 @@
+using Microsoft.EntityFrameworkCore;
+using MyPortfolıoUdemy.DAL.Context;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+// PostgreSQL bağlantısı
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+
+// Railway environment variables varsa onları kullan
+var pgHost = Environment.GetEnvironmentVariable("PGHOST");
+if (!string.IsNullOrEmpty(pgHost))
+{
+    var pgPort = Environment.GetEnvironmentVariable("PGPORT");
+    var pgDatabase = Environment.GetEnvironmentVariable("PGDATABASE");
+    var pgUser = Environment.GetEnvironmentVariable("PGUSER");
+    var pgPassword = Environment.GetEnvironmentVariable("PGPASSWORD");
+    
+    connectionString = $"Host={pgHost};Port={pgPort};Database={pgDatabase};Username={pgUser};Password={pgPassword};SSL Mode=Require;Trust Server Certificate=true";
+}
+
+builder.Services.AddDbContext<MyPortfolıoContext>(options =>
+    options.UseNpgsql(connectionString));
 
 var app = builder.Build();
 
@@ -9,20 +30,16 @@ var app = builder.Build();
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
-
 app.UseRouting();
-
 app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=About}/{action=AboutList}/{id?}");
-
 
 app.Run();
