f645ca7c303b187db68c9c25f5bda29962ee5f10
ipek nur <114228895+ipeknroztrk@users.noreply.github.com>
2025-11-29 13:24:45 +0300

    Update Program.cs

--- MyPortfolıoUdemy/Program.cs
+++ MyPortfolıoUdemy/Program.cs
@@ -1,36 +1,9 @@
-using Microsoft.EntityFrameworkCore;
-using MyPortfolıoUdemy.DAL.Context;
-
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
-// PostgreSQL bağlantısı
-var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
-
-var pgHost = Environment.GetEnvironmentVariable("PGHOST");
-if (!string.IsNullOrEmpty(pgHost))
-{
-    var pgPort = Environment.GetEnvironmentVariable("PGPORT");
-    var pgDatabase = Environment.GetEnvironmentVariable("PGDATABASE");
-    var pgUser = Environment.GetEnvironmentVariable("PGUSER");
-    var pgPassword = Environment.GetEnvironmentVariable("PGPASSWORD");
-    
-    connectionString = $"Host={pgHost};Port={pgPort};Database={pgDatabase};Username={pgUser};Password={pgPassword};SSL Mode=Require;Trust Server Certificate=true";
-}
-
-builder.Services.AddDbContext<MyPortfolıoContext>(options =>
-    options.UseNpgsql(connectionString));
-
 var app = builder.Build();
-
-// BURAYA EKLE - Otomatik migration
-using (var scope = app.Services.CreateScope())
-{
-    var db = scope.ServiceProvider.GetRequiredService<MyPortfolıoContext>();
-    db.Database.Migrate();
-}
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
