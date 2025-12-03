b3f7dc288f54a2b0e84151c62d6e72824e79a901
ipek nur <114228895+ipeknroztrk@users.noreply.github.com>
2025-12-03 17:53:09 +0300

    Create PortfolioController.cs

---
+++ MyPortfolıoUdemy/Controllers/PortfolioController.cs
@@ -0,0 +1,58 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using MyPortfolioUdemy.DAL.Context;   // namespace'i de i ile düzelt
+using MyPortfolioUdemy.DAL.Entities;  // namespace'i de i ile düzelt
+
+namespace MyPortfolioUdemy.Controllers
+{
+    public class PortfolioController : Controller
+    {
+        MyPortfolioContext context = new MyPortfolioContext();
+
+        public IActionResult PortfolioList()
+        {
+            var values = context.Portfolios.ToList();
+            return View(values);
+        }
+
+        [HttpGet]
+        public IActionResult CreatePortfolio()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult CreatePortfolio(Portfolio portfolio)
+        {
+            context.Portfolios.Add(portfolio);
+            context.SaveChanges();
+            return RedirectToAction("PortfolioList");
+        }
+
+        public IActionResult DeletePortfolio(int id)
+        {
+            var value = context.Portfolios.Find(id);
+            context.Portfolios.Remove(value);
+            context.SaveChanges();
+            return RedirectToAction("PortfolioList");
+        }
+
+        [HttpGet]
+        public IActionResult UpdatePortfolio(int id)
+        {
+            var value = context.Portfolios.Find(id);
+            return View(value);
+        }
+
+        [HttpPost]
+        public IActionResult UpdatePortfolio(Portfolio portfolio)
+        {
+            context.Portfolios.Update(portfolio);
+            context.SaveChanges();
+            return RedirectToAction("PortfolioList");
+        }
+    }
+}
