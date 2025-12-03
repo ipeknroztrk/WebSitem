24675606769baee1ccabc1aa4091b4f036080491
ipek nur <114228895+ipeknroztrk@users.noreply.github.com>
2025-12-03 17:40:33 +0300

    Fix class definition for Portfolio

--- MyPortfolıoUdemy/DAL/Entities/Portfolıo.cs
+++ MyPortfolıoUdemy/DAL/Entities/Portfolıo.cs
@@ -1,9 +1,14 @@
-public class Portfolio
+using System;
+
+namespace MyPortfolıoUdemy.DAL.Entities
 {
-    public int PortfolioId { get; set; }
-    public string Title { get; set; }
-    public string SubTitle { get; set; }
-    public string ImageUrl { get; set; }
-    public string Url { get; set; }
-    public string Description { get; set; }
+    public class Portfolio
+    {
+        public int PortfolioId { get; set; }
+        public string Title { get; set; }
+        public string SubTitle { get; set; }
+        public string ImageUrl { get; set; }
+        public string Url { get; set; }
+        public string Description { get; set; }
+    }
 }
